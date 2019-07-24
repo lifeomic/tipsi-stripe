532e39012398329c227429ff503794525ade5ff4
Charles Kenney <charlesc.kenney@gmail.com>
2019-07-24 14:23:29 -0400

    transpile source

--- example/scripts/pre-install.js
+++ example/scripts/pre-install.js
@@ -1,15 +1,24 @@
 #!/usr/bin/env node
-const path = require('path')
-const { execSync } = require('child_process')
-const { name, version } = require('../../package.json')
+const path = require("path");
+const { execSync } = require("child_process");
+const { name, version } = require("../../package.json");
 
-const rootDirectory = path.join(__dirname, '../..')
+const cleanName = name
+  .split(/[\/@]/g)
+  .filter(Boolean)
+  .join("-");
+
+const rootDirectory = path.join(__dirname, "../..");
 
 // 1. Remove all *.tgz before install
-execSync('rm -rf *.tgz', { cwd: rootDirectory })
+execSync("rm -rf *.tgz", { cwd: rootDirectory });
 
 // 2. Do npm pack
-execSync('npm pack', { cwd: rootDirectory })
+execSync("npm pack", { cwd: rootDirectory });
+
+console.log(cleanName);
 
 // 3. Rename .tgz to tipsi-stripe-latest.tgz
-execSync(`mv ${name}-${version}.tgz ${name}-latest.tgz`, { cwd: rootDirectory })
+execSync(`mv ${cleanName}-${version}.tgz ${cleanName}-latest.tgz`, {
+  cwd: rootDirectory
+});
